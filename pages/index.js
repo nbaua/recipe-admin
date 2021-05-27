b9008ccb239f9553dc778f1791633f1fc2357c9a
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-27 17:16:01 +0530

    minor code cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { useAppContext } from './../utils/app-context';
+
 function rootPage() {
+	const { activeSession } = useAppContext();
+
 	return (
 		<section className='hero is-success is-halfheight'>
 			<div className='hero-body'>
-				<div className=''>
+				<div>
 					<p className='title'>Weekly Dashboard</p>
 					<p className='subtitle'>Loading...</p>
 				</div>
