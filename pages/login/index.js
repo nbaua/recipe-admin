c89adb562f52a712d3826179bbf282977b468001
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-05-27 13:59:40 +0530

    fixed css classname property attribute

--- pages/login/index.js
+++ pages/login/index.js
@@ -7,22 +7,22 @@ function Login() {
 	return (
 		<div className='container section  mt-6'>
 			<div className='card'>
-				<form class='box'>
-					<div class='field'>
-						<label class='label'>Email</label>
-						<div class='control'>
-							<input class='input' type='email' placeholder='e.g. alex@example.com' />
+				<form className='box'>
+					<div className='field'>
+						<label className='label'>Email</label>
+						<div className='control'>
+							<input className='input' type='email' placeholder='e.g. alex@example.com' />
 						</div>
 					</div>
 
-					<div class='field'>
-						<label class='label'>Password</label>
-						<div class='control'>
-							<input class='input' type='password' placeholder='********' />
+					<div className='field'>
+						<label className='label'>Password</label>
+						<div className='control'>
+							<input className='input' type='password' placeholder='********' />
 						</div>
 					</div>
 
-					<button class='button is-primary'>Sign in</button>
+					<button className='button is-primary'>Sign in</button>
 				</form>
 			</div>
 		</div>
