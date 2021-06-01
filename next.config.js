b8ebd79985021d9089563796047049e2b626e3ee
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-06-01 19:28:55 +0530

    added webpack configuration

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ module.exports = {
 	images: {
 		domains: ['picsum.photos', 'loremflickr.com', 'unsplash.it'],
 	},
-}
+	future: {
+		webpack5: true,
+	},
+	webpack: function (config, options) {
+		console.log(options.webpack.version); // 5.18.0
+		config.experiments = {};
+		return config;
+	},
+};
