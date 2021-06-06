60d0289378baa00b4a28d03de431405589e200e1
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-06-06 12:06:13 +0530

    added footer control

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer class='footer'>
+			<div class='content has-text-centered'>&copy;Nhilesh Baua - 2020-{new Date().getFullYear()}</div>
+		</footer>
+	);
+};
+
+export default Footer;
