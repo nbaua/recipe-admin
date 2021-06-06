963b3a0554cdb5d9c6c19963ca6199ce94714f86
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-06-06 12:07:03 +0530

    refactored app, optimised main component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
+import Footer from '../components/footer';
+import Header from '../components/header';
 import '../styles/globals.css';
 import { AppContextProvider } from './../utils/app-context';
 
 function MyApp({ Component, pageProps }) {
-	let [isModal, setIsModal] = useState(false);
 	let [isLoggedIn, setIsLoggedIn] = useState(false);
 	const router = useRouter();
 
@@ -14,86 +13,20 @@ function MyApp({ Component, pageProps }) {
 		setIsLoggedIn(localStorage.getItem('token') !== '');
 	}, [router]);
 
-	const handleClick = () => {
-		setIsModal((isModal = !isModal));
-	};
-
 	const handleLogin = (e) => {
 		setIsLoggedIn(!isLoggedIn);
 		localStorage.removeItem('token');
 		router.push('/login');
 	};
 
-	const active = isModal ? 'is-active' : '';
 	return (
-		<section>
-			<AppContextProvider>
-				<div className={`modal ${active}`}>
-					<div className='modal-background' />
-					<div className='modal-card'>
-						<header className='modal-card-head'>
-							<p className='modal-card-title'>Recipe Admin - V1</p>
-							<button onClick={handleClick} className='delete' aria-label='close' />
-						</header>
-						<section className='modal-card-body'>
-							<div className='field'>
-								<label className='checkbox'>Recipe Admin is created using React library and NextJS framework With Bulma CSS.</label>
-								<div className='control mt-5'>Created by Nhilesh Baua, India</div>
-							</div>
-						</section>
-						<footer className='modal-card-foot'>
-							<button onClick={handleClick} className='button is-primary'>
-								Thanks
-							</button>
-						</footer>
-					</div>
-				</div>
-
-				<header>
-					<nav className='navbar is-dark is-fixed-top' role='navigation' aria-label='main navigation'>
-						<input type='checkbox' id='toggler' role='button' className='toggler' aria-label='menu' aria-expanded='false' data-target='mainBar' />
-						<div className='navbar-brand'>
-							<a className='navbar-item' href='/'>
-								<Image src='/logo.svg' width='129' height='36' />
-							</a>
-							<label className='navbar-burger burger'>
-								<span aria-hidden='true'></span>
-								<span aria-hidden='true'></span>
-								<span aria-hidden='true'></span>
-							</label>
-						</div>
-
-						<div id='mainBar' className='navbar-menu'>
-							<div className='navbar-start'>
-								<Link href='/'>
-									<a className='navbar-item'>Home</a>
-								</Link>
-								<Link href='/recipes'>
-									<a className='navbar-item'>Recipes</a>
-								</Link>
-							</div>
-							<div className='navbar-end'>
-								<div className='navbar-item'>
-									<div className='navbar-item has-dropdown is-hoverable'>
-										<a className='navbar-link'>{isLoggedIn ? 'Welcome Admin' : 'Welcome Anonymous'}</a>
-										<div className='navbar-dropdown'>
-											<a className='navbar-item' onClick={handleLogin}>
-												{isLoggedIn ? 'Logout' : 'Login'}
-											</a>
-											<hr className='navbar-divider' />
-											<a className='navbar-item' onClick={handleClick}>
-												About
-											</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</nav>
-				</header>
+		<AppContextProvider>
+			<Header isLoggedIn={isLoggedIn} />
+			<section style={{ minHeight: '95vh', marginBottom: -60, paddingTop: 20 }}>
 				<Component {...pageProps} />
-			</AppContextProvider>
-		</section>
+			</section>
+			<Footer />
+		</AppContextProvider>
 	);
 }
 
